79b98f064a0c817af9215c0dca29a02e913e2f60
cysbyte <chenyunshu2@gmail.com>
2024-10-25 15:14:58 +0800

    fix: fixed bug

--- src/components/home/NftLaunch.tsx
+++ src/components/home/NftLaunch.tsx
@@ -4,9 +4,9 @@ import bg2 from '../../assets/home/nft-launch/bg2-md.svg'
 
 const NftLaunch = () => {
   return (
-    <section className='w-full my-4 md:my-20'>
-        <img className='hidden md:block' src={nftLaunch} alt=''/>
-        <div className='block md:hidden relative w-full'>
+    <section className='w-full my-4 md:my-20 '>
+        <img className='hidden md:block mx-auto' src={nftLaunch} alt=''/>
+        <div className='block md:hidden relative w-full mx-auto'>
             <img className='absolute w-full top-0 left-0' src={bg2} alt=''/>
             <img className='absolute w-full top-0 left-0' src={bg1} alt=''/>
         </div>
